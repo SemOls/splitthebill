2f5c5346e967b95c899f37261142243366d852dd
Glitch (glitch-hello-website) <none>
2022-04-06 09:26:50 +0000

    🐸👀 Checkpoint ./index.html:38693240/6182 ./style.css:38693240/5516 ./script.js:38693240/4021 ./.glitch-assets:38693240/3346

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const pictures = [
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/1.jpeg?v=1649236700326', delay: 1000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/2.jpeg?v=1649236705171', delay: 2000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/3.jpeg?v=1649236708620', delay: 3000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/4.jpeg?v=1649236712497', delay: 4000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/5.jpeg?v=1649236716845', delay: 5000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/6.jpeg?v=1649236720952', delay: 6000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/7.jpeg?v=1649236724869', delay: 6000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/8.jpeg?v=1649236728308', delay: 6000 },
+    { src: 'https://cdn.glitch.global/8a3da3df-d914-4192-a3bf-25addd037603/9.jpeg?v=1649236731591', delay: 6000 },
+  ]
+  
+  let current  = 0;
+  
+  function nextImage () {
+    let img = new Image;
+    img.src = pictures[current].src;
+    img.onload = function () {
+      document.body.style.backgroundImage = 'url(' + pictures[current].src + ')';
+  
+      current++;
+      if (current >= pictures.length) current = 0;
+  
+      setTimeout(nextImage, pictures[current].delay);
+    }
+  }
+  
+  nextImage();
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+  const buttonRoom=document.querySelector("#addRoom");
+  buttonRoom.addEventListener("click", showRoom);
+  function showRoom (e) {
+    e.preventDefault();
+    room.style.display="block";
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+const buttonEat=document.querySelector("#addEat");
+buttonEat.addEventListener("click", showEat);
+function showEat (e) {
+  e.preventDefault();
+  eat.style.display="block";
+}
+
+
+  const button =document.querySelector("#btn");
+  button.addEventListener ("click", calculateAmount)
+
+  function calculateAmount (e) {
+      e.preventDefault ();
+
+      const people = document.querySelector("#people").value;
+      const night = document.querySelector("#night").value;
+      const room = document.querySelector("#room").value;
+      const eat = document.querySelector("#eat").value;
+
+    if (people ==="" || people <1 || night === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'Please enter your information!',
+                      });
+            }
+    if (people > 6) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Sorry...',
+            text: 'We have rooms just for 6 person 🙏',
+                      });
+    }
+
+        
+let amountPerPerson = (room * night)/people;
+
+let eatPerPerson = (eat * night )/people
+
+ let totalSum = amountPerPerson+eatPerPerson;
+
+
+document.querySelector("#divideBill").textContent=amountPerPerson.toFixed(2);
+
+document.querySelector("#divideEat").textContent=eatPerPerson.toFixed(2);
+
+document.querySelector("#billAndEat").textContent=totalSum.toFixed(2);
+
+  }
